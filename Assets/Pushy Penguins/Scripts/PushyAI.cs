605ca31863bf461b0a88d09ac28ed8d9bd5f36de
Shafournee <31718336+Shafournee@users.noreply.github.com>
2018-03-19 22:11:29 -0400

    Added some pushy penguin shit

---
+++ Assets/Pushy Penguins/Scripts/PushyAI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushyAI : MonoBehaviour {
+
+    Rigidbody2D rigidBody;
+    float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rigidBody.velocity = new Vector2(-speed, 0f);
+	}
+}
---
+++ Assets/Pushy Penguins/Scripts/PushyPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushyPlayer : MonoBehaviour
+{
+
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode left;
+    public KeyCode right;
+    Rigidbody2D rigidBody;
+    float speed = 5f;
+    public Color color;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        GetComponent<SpriteRenderer>().color = color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(up))
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, speed);
+        }
+        else if (Input.GetKey(down))
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, -speed);
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, 0f);
+        }
+        if (Input.GetKey(left))
+        {
+            rigidBody.velocity = new Vector2(-speed, rigidBody.velocity.y);
+        }
+        else if (Input.GetKey(right))
+        {
+            rigidBody.velocity = new Vector2(speed, rigidBody.velocity.y);
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(0f, rigidBody.velocity.y);
+        }
+    }
+}
